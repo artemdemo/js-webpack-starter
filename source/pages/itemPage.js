9325451baf55a98008e6491f7f053f9c7d9f9700
Artem Demo <artemdemo@gmail.com>
2018-04-06 12:47:09 +0300

    removed console.log

--- source/pages/itemPage.js
+++ source/pages/itemPage.js
@@ -5,8 +5,6 @@ import { addToCart } from '../model/cart/cartActions';
 const itemPage = (itemId) => {
     const { items } = store.getState();
     const item = items.data.find(_item => _item.id === itemId);
-    console.log(items.data);
-    console.log(item);
     return html`
         <h3>${item.name}</h3>
         <p>${item.description}</p>
